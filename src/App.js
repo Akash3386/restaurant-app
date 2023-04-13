05ecd3fcc35325f15cc1b0439bc6a6c4c4b3fa6c
Akash3386 <pandey.akash3386@gmail.com>
2023-04-13 10:11:43 +0530

    adding summary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 import Header from "./components/Layout/Header";
+import MealSummary from "./components/Meals/MealsSummary";
 
 function App() {
   return (
     <Fragment>
       {/* <h2>Let's get started!</h2> */}
       <Header/>
+      <MealSummary/>
     </Fragment>
   );
 }
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,15 @@
+import classes from './MealSummary.module.css'
+
+const MealSummary = () => {
+    return <section className={classes.summary}>
+        <h2>Delicious Food, Delivered to you</h2>
+        <p>Choose your favorite meal from our board section is available<br/> 
+            and enjoy a delicious lunch</p>
+
+        <p>All our meal is cooked with high quality ingredients, jsut in a time, 
+            and ofcourse by experienced chef</p>
+    </section>
+
+}
+
+export default MealSummary
